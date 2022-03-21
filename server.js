5ab0b20d49271aafa360b2bc130f028db782273b
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-03-21 20:49:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(__dirname + '/client/build'));
 
 	app.get('*', (request, response) => {
-		response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		response.sendFile(Path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
