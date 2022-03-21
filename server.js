38f01b880fcffc74907ea6538c484579ff151f31
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-03-21 20:27:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const cors = require("cors");
-const path = require("path");
+const Path = require("path");
+const Fs = require("fs");
 const { Server } = require("socket.io");
 const { pipeline } = require("serialport");
 const SerialPort = require("serialport");
@@ -37,7 +38,6 @@ const io = new Server(server, {
 // 	delimiter: '\r\n'
 // });
 		
-var allowed = 0;
 
 app.get("/", (request, response) => {
 	response.writeHead(200, {"Content-Type": "text/html"});
@@ -66,22 +66,58 @@ io.on('connection', (socket) => {
 	socket.on('result', (data) => {
 		if (data.status === 1) {
 			console.log("writing HIGH...");
-			allowed = 1;
+
+			axios.get('http://192.168.180.70/isallowed/1')
+				.then((response) => {
+					console.log(response.data);
+				})
 			// port.write("1");
-		} else {
+		} else if (data.status === 2) {
 			console.log("writing LOW...");
-			allowed = 0;
+
+			axios.get('http://192.168.180.70/isallowed/0')
+				.then((response) => {
+					console.log(response.data);
+				})
 			// port.write("0");
 		}
 
-		axios.get('http://192.168.180.161/1600x1200.jpg', {responseType: 'arraybuffer'})
-			.then(response => {
-				const buffer = Buffer.from(response.data, 'binary').toString('base64')
-				console.log(buffer);
-			})
-			.catch(error => {
-				console.log(error);
-			})
+		// const path = Path.resolve(__dirname, 'files', 'image.jpg');
+		// const pathModel = Path.resolve('files', 'image.jpg');
+
+		// axios.get('http://192.168.180.161/1600x1200.jpg', {responseType: 'stream'})
+		// 	.then(response => {
+		// 		response.data.pipe(Fs.createWriteStream(path));
+
+		// 		return new Promise((resolve, reject) => {
+		// 			response.data.on('end', () => {
+		// 				console.log('image downloaded successfully');
+
+		// 				let model = new cvtfjs.ObjectDetectionModel();
+		// 				model.loadModelAsync('model.json')
+		// 					.then(() => {
+		// 						const image = Fs.createReadStream(pathModel);
+		// 						console.log(image);
+		// 						model.executeAsync(image)
+		// 							.then((result) => {
+		// 								console.log(result);
+		// 							})
+		// 					});
+
+		// 				resolve();
+		// 			})
+		// 		})
+
+		// 		response.data.on('error', (error) => {
+		// 			reject(error);
+		// 		})
+		// 		// console.log(response);
+		// 		// const buffer = Buffer.from(response.data, 'binary').toString('base64')
+		// 		// console.log(buffer);
+		// 	})
+		// 	.catch(error => {
+		// 		console.log(error);
+		// 	})
 
 		socket.emit('recieved', {data: data, port: Port});
 	});
