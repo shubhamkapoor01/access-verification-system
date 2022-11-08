630051cc402d4198116e9643ebd1a3f937344e57
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-11-08 17:11:21 +0530

    Update hardhat.config.js

--- client/hardhat.config.js
+++ client/hardhat.config.js
@@ -28,7 +28,7 @@ module.exports = {
       chainId: 1337
     },
     ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`]
     },
     rinkeby: {
